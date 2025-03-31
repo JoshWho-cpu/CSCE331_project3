7dc4ee629cae622995bed0bab27ba3f8a70e55f1
jbw0199-tamu <joshwu820@gmail.com>
2025-03-31 17:36:04 -0500

    start and config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default {
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 5173,
-    allowedHosts: ['csce331-project3-cuxy.onrender.com']
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  preview: {
+    allowedHosts: ["csce331-project3-1.onrender.com"], // Add your Render host here
+    port: process.env.PORT || 5173, // Ensure it respects the assigned port
+    host: true, // Allow external access
   }
-};
+})
